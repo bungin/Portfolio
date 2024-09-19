81b4137a5378ec32532d1532b3978a14aaf1b60d
bungin <nimai.band@gmail.com>
2024-09-19 03:01:16 -0500

    footer img fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import '../components/css/Footer.css';
+import imgFooter from './src/assets/github-mark.png';
+
+function FooterImg() {
+  return (
+    <img src={imgFooter} alt="NimMac/Bungin GitHub link" />
+  );
+}
 
 function Footer() {
   return (
     <footer className="footer">
       <a href="https://github.com/bungin/">
-        <button>
-          <img className='gitButt' src="./public/github-mark.png" alt="NimMac/Bungin GitHub link" />
+        <button >
+          <FooterImg />
         </button>
       </a>
       &copy; {new Date().getFullYear()} NimMac/Bungin
