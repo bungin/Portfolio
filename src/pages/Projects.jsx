e0f47dafad84c4b669454c8918c56947ae8b0727
bungin <nimai.band@gmail.com>
2024-09-19 04:42:44 -0500

    fix for imgs failing build, removed empty folder

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import "../components/css/Projects.css";
-import img1 from "../pages/assets/weatherweek-api.png";
-import img2 from "../pages/assets/vehicularInquirer.png";
-import img3 from "../pages/assets/CMSSQL.png";
-import img4 from "../pages/assets/blogger.png";
-import img5 from "../pages/assets/READMEGEN.png";
+import img1 from "/weatherweek-api.png";
+import img2 from "/vehicularInquirer.png";
+import img3 from "/CMSSQL.png";
+import img4 from "/blogger.png";
+import img5 from "/READMEGEN.png";
 
 const imgArray = [img1, img2, img3, img4, img5];
 
